47f4d50771237b990894d30ff5bc26847f771013
Vladislav Kramorenko <85196001+Xewus@users.noreply.github.com>
2022-08-16 08:52:57 +0300

    Update views.py

--- app/users/views.py
+++ app/users/views.py
@@ -7,5 +7,7 @@ from user_sessions.backends_db import UserSession
 class ChangePasswordView(PasswordChangeView):
 
     def form_valid(self, form) -> HttpResponse:
+        """Удаляет все сессии пользователя при смене пароля.
+        """
         UserSession.objects.filter(user_id=self.request.user.id).delete()
         return super().form_valid(form)
